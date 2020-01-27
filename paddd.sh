842eddb8c77e3ac5a48c9393dde254750ed042d4
SonicSparks <14578906+SonicSparks91@users.noreply.github.com>
2020-01-27 00:30:49 -0500

    v28

--- paddd.sh
+++ paddd.sh
@@ -593,8 +593,9 @@ PrintNetworkInformation() {
   if [ "$1" = "pico" ]; then
     echo "${bold_text}NETWORK/SYSTEM ================${reset_text}"
     echo -e " Host: ${pi_hostname}  DHCP ${dhcp_check_box} IPv6 ${dhcp_ipv6_check_box}"
-    echo -e " IPv4: ${IPV4_ADDRESS} %-10s${temp_heatmap}%-20s${reset_text}" "CPU Temp:" "${temperature}"
-	printf " Load: [${cpu_load_1_heatmap}${cpu_bar}${reset_text}] ${cpu_percent}%"
+    echo -e " IPv4: ${IPV4_ADDRESS}  Load:  [${cpu_load_1_heatmap}${cpu_bar}${reset_text}] ${cpu_percent}%"
+    printf " %-10s${temp_heatmap}%-20s${reset_text}" "CPU Temp:" "${temperature}"
+
 	
   elif [ "$1" = "nano" ]; then
     echo "${bold_text}NETWORK ================${reset_text}"
