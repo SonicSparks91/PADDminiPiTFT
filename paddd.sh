6ee6ba3663f79c3b6d2f73726eccd03745a9ae5d
SonicSparks <14578906+SonicSparks91@users.noreply.github.com>
2020-01-27 00:42:55 -0500

    v31

--- paddd.sh
+++ paddd.sh
@@ -594,7 +594,7 @@ PrintNetworkInformation() {
     echo "${bold_text}NETWORK/SYSTEM ================${reset_text}"
     echo -e " Host: ${pi_hostname}  DHCP ${dhcp_check_box} IPv6 ${dhcp_ipv6_check_box}"
     echo -e " IPv4: ${pi_ip_address}Load:[${cpu_load_1_heatmap}${cpu_bar}${reset_text}] ${cpu_percent}%"
-    printf " %-10s${temp_heatmap}%-20s${reset_text}" "CPU Temp:" "${temperature}"
+    echo -e "CPU Temp:" "${temperature}"
 
   elif [ "$1" = "nano" ]; then
     echo "${bold_text}NETWORK ================${reset_text}"
